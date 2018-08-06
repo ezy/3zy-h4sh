a987912397a3a20a182e92c2fc0361f36f9029dc
Ezra Keddell <ezrakeddell@gmail.com>
2018-08-06 12:52:57 +1200

    tru3 1337n3ss

--- bot.js
+++ bot.js
@@ -1,21 +1,23 @@
 // Our Twitter library
-var Twit = require('twit');
-
-// We need to include our configuration file
-var T = new Twit(require('./config.js'));
+const Twit = require('twit');
 const crypto = require('crypto');
+const l33t = require('leet');
+const faker = require('faker');
+const T = new Twit(require('./config.js'));
 
-// Generate a hash
-function genHash() {
-  const current_date = (new Date()).valueOf().toString();
-  const random = Math.random().toString();
-  const hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
-  return hash;
-};
+// // Generate a hash
+// function genHash() {
+//   const current_date = (new Date()).valueOf().toString();
+//   const random = Math.random().toString();
+//   const hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+//   return hash;
+// };
 
 // This function tweets the created hash in genHash
 function tweetHash() {
-  const tweet = genHash();
+  const phrase = faker.hacker.phrase();
+  const tweet = l33t.convert(phrase);
+
   T.post('statuses/update', { tweet }, (err, data, response) => {
     if (response) {
       console.log('Success! Check your bot, it should have tweeted a hash.');
@@ -31,4 +33,4 @@ function tweetHash() {
 tweetHash();
 // ...and then every 30 mins after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 30 = 30 minutes --> 1000 * 60 * 30
-setInterval(tweetHodor, 1000 * 60 * 30);
+setInterval(tweetHash, 1000 * 60 * 30);
